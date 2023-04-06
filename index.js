2e93b9b299a0e2863472af7cc0e508daf317ca51
Vladislav Urykov <vladislavurykov29@gmail.com>
2023-04-06 14:21:39 +0300

    client js added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const chalk = require("chalk");
+const path = require("path");
 
-const { addNote, getNotes } = require("./notes.controller");
+const { addNote, getNotes, removeNote } = require("./notes.controller");
 
 const port = 3000;
 
@@ -9,15 +10,33 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "pages");
+app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  res.render("index", { title: "Express App", notes: await getNotes() });
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
 app.post("/", async (req, res) => {
   await addNote(req.body.title);
-  res.render("index", { title: "Express App", notes: await getNotes() });
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: true,
+  });
+});
+
+app.delete("/:id", async (req, res) => {
+  await removeNote(req.params.id);
+  res.render("index", {
+    title: "Express App",
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
 app.listen(port, () => {
