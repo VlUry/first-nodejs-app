3c517eb87b59c87b85f9673dd3b90d428287a350
Vladislav Urykov <vladislavurykov29@gmail.com>
2023-04-06 08:19:10 +0300

    getting html file

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const http = require("http");
 const chalk = require("chalk");
+const fs = require("fs/promises");
+const path = require("path");
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log("Request method:", req.method);
+const basePath = path.join(__dirname, "./pages");
 
-  res.end("Privet");
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET") {
+    const content = await fs.readFile(path.join(basePath, "index.html"));
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(content);
+  }
 });
 
 server.listen(port, () => {
